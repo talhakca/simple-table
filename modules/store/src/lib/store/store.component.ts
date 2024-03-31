2ded50b7d12f3c6f9608df685a92e6fc42cf68e7
Talha Akca <talhakca@gmail.com>
2024-03-31 17:35:40 +0300

    store lib added

---
+++ modules/store/src/lib/store/store.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-store',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './store.component.html',
+  styleUrl: './store.component.css',
+})
+export class StoreComponent {}
